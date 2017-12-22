3d72a1dd86dbd8c7f3f465bafe7794f1c39caf19
gustavocd <gustavocastillo1993@hotmail.com>
2017-12-21 21:17:19 -0600

    Enable and organize Sass files

---
+++ src/EmailClient.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles/styles.css';
+
+class EmailClient extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default EmailClient;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EmailClient from './EmailClient';
+
+ReactDOM.render(<EmailClient />, document.getElementById('root'));
